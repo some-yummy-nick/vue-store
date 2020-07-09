980b78c71fcdbf207b4272041d54b925846022d9
Marat <yeffasol@mail.ru>
2020-07-09 12:21:43 +0300

    Add register form

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,10 +1,51 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Register</h1>
-      </v-flex>
-    </v-layout>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Registration form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                label="Email"
+                name="Email"
+                prepend-icon="mdi-account"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+
+              <v-text-field
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="password"
+                :counter="6"
+                :rules="passwordRules"
+              ></v-text-field>
+              <v-text-field
+                label="Confirm Password"
+                name="confirm-password"
+                prepend-icon="mdi-repeat"
+                type="password"
+                v-model="confirmPassword"
+                :counter="6"
+                :rules="confirmPasswordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="onSubmit" :disabled="!valid"
+              >Create account</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -12,7 +53,36 @@
 export default {
   name: 'Register',
   data() {
-    return {}
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v =>
+          (v && v.length >= 6) ||
+          'Password must be equal or more than 6 characters',
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match',
+      ],
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password,
+        }
+      }
+    },
   },
 }
 </script>
