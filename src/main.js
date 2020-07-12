bde7abb2a94ad8256f48ac6ba6dec92acda10327
Marat <yeffasol@mail.ru>
2020-07-12 10:50:14 +0300

    Add firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import * as fb from 'firebase/app'
 
 import store from './store'
 
@@ -12,4 +13,17 @@ new Vue({
   vuetify,
   store,
   render: h => h(App),
+  created() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDOCAbC123dEf456GhI789jKl01-MnO',
+      authDomain: 'myapp-project-123.firebaseapp.com',
+      databaseURL: 'https://myapp-project-123.firebaseio.com',
+      projectId: 'myapp-project-123',
+      storageBucket: 'myapp-project-123.appspot.com',
+      messagingSenderId: '65211879809',
+      appId: '1:65211879909:web:3ae38ef1cdcb2e01fe5f0c',
+      measurementId: 'G-8GSGZQ44ST',
+    }
+    fb.initializeApp(firebaseConfig)
+  },
 }).$mount('#app')
